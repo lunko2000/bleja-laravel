d10ff376508fafad5511cefa62aefd17bc98dabf
lunko2000 <milanv.vasiljevic@gmail.com>
2025-03-11 14:34:50 +0100

    Rverted from Railway to Heroku for deploying the app

--- check-db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
-    echo "Database connection successful\n";
-} catch (PDOException $e) {
-    file_put_contents('/var/www/html/storage/logs/db-error.log', 'Connection failed: ' . $e->getMessage() . "\n", FILE_APPEND);
-    exit(1);
-}
